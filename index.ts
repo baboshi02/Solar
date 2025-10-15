e643c8297010f32d6881a339f1e7e53ecd0e65ce
baboshi <drhameid@yahoo.com>
2025-10-15 12:21:20 +0200

    Basic changes done

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
-import {
-  TelegramBot,
-  KeyboardButton,
-  ReplyKeyboardMarkup,
-} from "typescript-telegram-bot-api";
+import { TelegramBot } from "typescript-telegram-bot-api";
 import dotenv from "dotenv";
 import { UserStates } from "./interfaces/userStates";
-import { id_command, load_command, start_command } from "./commands";
+import { load_command, start_command } from "./commands";
 import { Load } from "./interfaces/loads";
 
 dotenv.config();
@@ -22,48 +18,62 @@ bot.on("message:text", (msg) => {
   const user_id = msg.from?.id;
   const chat_id = msg.chat.id;
   const msg_text = msg.text;
-  if (!user_id) {
-    const text =
-      "This seems to be a channel this service only works for accounts";
-    console.log("loads: ", loads);
-    bot.sendMessage({ chat_id, text });
-    return;
-  }
-  if (userStates[user_id] == 1) {
-    //TODO: Make the monsumage forced to be a number
-    loads[user_id] = { name: msg_text, power: 0 };
-    bot.sendMessage({ chat_id, text: "Enter the power consumage" });
-    userStates[user_id] = 2;
-    return;
-  }
-  if (userStates[user_id] == 2) {
-    loads[user_id].power = Number(msg_text);
-    const load_name = loads[user_id].name;
-    const load_power = loads[user_id].power;
-    const text = `Your ${load_name} have power of ${load_power}`;
+  try {
+    if (!user_id) {
+      const text =
+        "This seems to be a channel this service only works for accounts";
+      console.log("loads: ", loads);
+      bot.sendMessage({ chat_id, text });
+      return;
+    }
+    if (userStates[user_id] == "initial") {
+      if (msg_text === "add") {
+        const text = "Enter the load name ";
+        bot.sendMessage({ chat_id, text });
+        userStates[user_id] = "add_load";
+        return;
+      }
+      if (msg_text === "show") {
+        const text = "These are the available loads";
+        userStates[user_id] = "show_loads";
+        bot.sendMessage({ chat_id, text });
+        return;
+      }
+    }
+    if (userStates[user_id] == "add_load") {
+      //TODO: Make the monsumage forced to be a number
+      loads[user_id] = { name: msg_text, power: 0 };
+      bot.sendMessage({ chat_id, text: "Enter the power consumage" });
+      userStates[user_id] = "add_consumage";
+      return;
+    }
+    if (userStates[user_id] == "add_consumage") {
+      loads[user_id].power = Number(msg_text);
+      const load_name = loads[user_id].name;
+      const load_power = loads[user_id].power;
+      const text = `Your ${load_name} have power of ${load_power}`;
+      bot.sendMessage({
+        chat_id,
+        text,
+      });
+      userStates[user_id] = "start";
+      return;
+    }
+    if (msg_text == "/start") {
+      start_command(bot, msg);
+      return;
+    }
+    if (msg_text == "/loads") {
+      load_command(bot, msg, userStates);
+      return;
+    }
+    const text = "Unknown text";
     bot.sendMessage({
-      chat_id,
+      chat_id: msg.chat.id,
       text,
     });
-    userStates[user_id] = 0;
-    return;
+  } catch (error) {
+    console.error(error);
+    bot.sendMessage({ chat_id, text: "Unknown error occured" });
   }
-  if (msg_text == "/start") {
-    start_command(bot, msg);
-    return;
-  }
-  if (msg_text == "/id") {
-    id_command(bot, msg);
-    return;
-  }
-  if (msg_text == "/loads") {
-    load_command(bot, msg, userStates);
-
-    return;
-  }
-  const text = "Unknown text";
-  bot.sendMessage({
-    chat_id: msg.chat.id,
-    text,
-  });
 });
